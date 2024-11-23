8dc17da351f17f17084e601fb27ccb36bfe8c495
Mintu Sikder <mintusikder15@gmail.com>
2024-11-23 20:19:22 +0600

    added navbar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,76 @@
+import { useContext } from "react";
+import logo from "../assets/images/logo.jpg";
+import { AuthContext } from "../provider/AuthProvider";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  const { user, logOut } = useContext(AuthContext);
+  return (
+    <div className="navbar bg-base-100 shadow-sm container px-4 mx-auto">
+      <div className="flex-1">
+        <div className="flex gap-2 items-center">
+          <Link to="/">
+            {" "}
+            <img className="w-auto h-7" src={logo} alt="" />
+          </Link>
+          <Link to="/">
+            {" "}
+            <span className="font-bold">Book Library</span>
+          </Link>
+        </div>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Book</Link>
+          </li>
+
+          <li>
+            <Link to="/dashboard/book">Dashboard</Link>
+          </li>
+        </ul>
+        {user && (
+          <div className="dropdown dropdown-end z-50">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full" title={user?.displayName}>
+                <img
+                  referrerPolicy="no-referrer"
+                  alt="User Profile Photo"
+                  src={user?.photoURL}
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <Link to="/addBook">
+                <div className="justify-between">Add Book</div>
+              </Link>
+              <Link to="/books">
+                <div>My Book</div>
+              </Link>
+
+              <li className="mt-2">
+                <button
+                  onClick={logOut}
+                  className="bg-gray-200 block text-center"
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
